3cdae0d805871f6c8af75999852ca73c0c8d798a
borisov <a.borisov@8bitgroup.com>
2020-07-17 10:36:46 +0300

    Add server interceptors

--- src/main/java/org/camunda/bpm/engine/grpc/server/configuration/ServerConfiguration.java
+++ src/main/java/org/camunda/bpm/engine/grpc/server/configuration/ServerConfiguration.java
@@ -1,6 +1,7 @@
 package org.camunda.bpm.engine.grpc.server.configuration;
 
 import io.grpc.Server;
+import io.grpc.ServerInterceptor;
 import io.grpc.netty.shaded.io.grpc.netty.InternalProtocolNegotiators;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -20,20 +22,23 @@ public class ServerConfiguration {
     @Bean
     public Server getGrpcServer(
         GrpcService grpcService,
-        ServerConfigurationProperties properties
+        ServerConfigurationProperties properties,
+        List<ServerInterceptor> serverInterceptors
     ) throws IOException {
         log.info("Starting GRPC server on port '{}'...", properties.getPort());
 
-        final Server server =
-            NettyServerBuilder
-                .forPort(properties.getPort())
-                .addService(grpcService)
-                .permitKeepAliveWithoutCalls(true)
-                .permitKeepAliveTime(properties.getPermitKeepAlive(), TimeUnit.SECONDS)
-                .keepAliveTime(properties.getKeepAlive(), TimeUnit.SECONDS)
-                .keepAliveTimeout(properties.getKeepAliveTimeout(), TimeUnit.SECONDS)
-                .protocolNegotiator(InternalProtocolNegotiators.plaintext())
-                .build();
+        NettyServerBuilder builder = NettyServerBuilder
+            .forPort(properties.getPort())
+            .addService(grpcService)
+            .permitKeepAliveWithoutCalls(true)
+            .permitKeepAliveTime(properties.getPermitKeepAlive(), TimeUnit.SECONDS)
+            .keepAliveTime(properties.getKeepAlive(), TimeUnit.SECONDS)
+            .keepAliveTimeout(properties.getKeepAliveTimeout(), TimeUnit.SECONDS)
+            .protocolNegotiator(InternalProtocolNegotiators.plaintext());
+
+        serverInterceptors.forEach(builder::intercept);
+
+        final Server server = builder.build();
 
         server.start();
         log.info("GRPC server started successfully on port '{}'", properties.getPort());
